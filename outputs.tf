c1cbc3aeea8a5ff05f45b3d5b944a3ed7daafb23
vinod <mechvnod@gmail.com>
2025-12-14 14:49:41 +0530

    terraform

--- outputs.tf
+++ outputs.tf
@@ -143,3 +143,138 @@ output "tags" {
     value = aws_instance.vinod_ec2.tags
 }
 
+output "public_ip" {
+    value = aws_instance.vinod_ec2.public_ip
+    
+}
+
+output "private_ip" {
+    value = aws_instance.vinod_ec2.private_ip
+}
+
+output "ami_id" {
+    value = aws_instance.vinod_ec2.ami_id
+}
+
+output "vpc_security_group_ids" {
+    value = vpc_security_group_ids
+}
+
+output "vpc_security_group_ids" {
+    value = vpc_security_group_ids
+}
+
+output "tags" {
+    value = aws_instance.vinod_ec2.tags
+}
+
+output "public_ip" {
+    value = aws_instance.vinod_ec2.public_ip
+    
+}
+
+output "private_ip" {
+    value = aws_instance.vinod_ec2.private_ip
+}
+
+output "ami_id" {
+    value = aws_instance.vinod_ec2.ami_id
+}
+
+output "vpc_security_group_ids" {
+    value = vpc_security_group_ids
+}
+
+output "vpc_security_group_ids" {
+    value = vpc_security_group_ids
+}
+
+output "tags" {
+    value = aws_instance.vinod_ec2.tags
+}output "public_ip" {
+    value = aws_instance.vinod_ec2.public_ip
+    
+}
+
+output "private_ip" {
+    value = aws_instance.vinod_ec2.private_ip
+}
+
+output "ami_id" {
+    value = aws_instance.vinod_ec2.ami_id
+}
+
+output "vpc_security_group_ids" {
+    value = vpc_security_group_ids
+}
+
+output "vpc_security_group_ids" {
+    value = vpc_security_group_ids
+}
+
+output "tags" {
+    value = aws_instance.vinod_ec2.tags
+}
+output "public_ip" {
+    value = aws_instance.vinod_ec2.public_ip
+    
+}
+
+output "private_ip" {
+    value = aws_instance.vinod_ec2.private_ip
+}
+
+output "ami_id" {
+    value = aws_instance.vinod_ec2.ami_id
+}
+
+output "vpc_security_group_ids" {
+    value = vpc_security_group_ids
+}
+
+output "vpc_security_group_ids" {
+    value = vpc_security_group_ids
+}
+
+output "tags" {
+    value = aws_instance.vinod_ec2.tags
+}
+
+output "public_ip" {
+    value = aws_instance.vinod_ec2.public_ip
+    
+}
+
+output "private_ip" {
+    value = aws_instance.vinod_ec2.private_ip
+}
+
+output "ami_id" {
+    value = aws_instance.vinod_ec2.ami_id
+}
+
+output "vpc_security_group_ids" {
+    value = vpc_security_group_ids
+}
+
+output "vpc_security_group_ids" {
+    value = vpc_security_group_ids
+}
+
+output "tags" {
+    value = aws_instance.vinod_ec2.tags
+}output "public_ip" {
+    value = aws_instance.vinod_ec2.public_ip
+    
+}
+
+output "private_ip" {
+    value = aws_instance.vinod_ec2.private_ip
+}
+
+output "ami_id" {
+    value = aws_instance.vinod_ec2.ami_id
+}
+
+
+
