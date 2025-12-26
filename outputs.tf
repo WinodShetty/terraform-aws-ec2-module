8e03bc9a3762765109d6263eb76bc7adfa4cbfb2
vinod <mechvnod@gmail.com>
2025-12-26 17:33:16 +0530

    terraform

--- outputs.tf
+++ outputs.tf
@@ -9055,5 +9055,14 @@ output "tags" {
     value = aws_instance.vinod_ec2.public_ip
     
 }
+output "vpc_security_group_ids" {
+    value = vpc_security_group_ids
+}
 
+output "tags" {
+    value = aws_instance.vinod_ec2.tags
+}output "public_ip" {
+    value = aws_instance.vinod_ec2.public_ip
+    
+}
 
